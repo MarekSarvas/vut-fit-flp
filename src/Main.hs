965d55aa2d5eddfb3f84787d0311e5a1ce84c652
Marek Sarvas <sarvas.sarvas@gmail.com>
2022-03-16 17:47:26 +0100

    Argument parsing

--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,16 @@ import System.Environment
 
 main :: IO ()
 main = do 
-    s <- parseArgs <$> getArgs
-    putStrLn s
+    (action, file_path) <- parseArgs <$> getArgs
+    putStrLn action
+    putStrLn file_path
     return ()
 
-parseArgs :: [String] -> String
-parseArgs _ = "hello"
+parseArgs :: [String] -> (String, String) 
+parseArgs [] = error "No arguments" 
+parseArgs [x] 
+    | x `elem` ["-i", "1", "2"] = (x, "STDIN")
+    | otherwise = error "Wrong first argument"
+parseArgs (x:y:_) 
+    | x `elem` ["-i", "1", "2"] = (x, y)
+    | otherwise = error "Wrong first argument"
