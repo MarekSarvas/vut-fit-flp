db85af1d99fc9b93c67d914ff4224583c2f14537
MarekSarvas <sarvas.sarvas@gmail.com>
2022-04-02 01:38:52 +0200

    almost first algo

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 import System.Environment   
 import Text.Parsec (char, endBy, eof, many1, newline, oneOf, parse, ParseError, sepBy1, string)
 import Text.Parsec.String (Parser)
+import Data.List (nub)
 
 -- Data types for Grammar representation
 type Nonterminal = String
@@ -13,6 +14,12 @@ type Terminals = [Terminal]
 type Rule = (Nonterminal, [String])
 type Rules = [Rule] 
 
+type RuleString = [String]
+
+type NaSet = (Nonterminal, Nonterminals)
+type NaSets = [NaSet]
+
+
 data Grammar = Grammar
     { nonterminals :: Nonterminals,
       terminals :: Terminals,
@@ -20,7 +27,7 @@ data Grammar = Grammar
       rules :: [Rule]
     } deriving (Show)
 
-
+-- ============= PARSER ====================
 -- Parse correct Nonterminal input 
 parseNonTerminals :: Parser Nonterminals 
 parseNonTerminals = sepBy1 parseOneNonTerminal (char ',') <* newline --read until \n
@@ -65,10 +72,44 @@ parseGrammar'  = do
 -- foldl pre rules a vo funkcii && ci je elementom nonterms ++ terms 
 checkSemantics :: Grammar -> Grammar
 checkSemantics g = 
-    if startNonterm g `elem` nonterminals g then g 
-    else error "Invalid starting symbol"
+    if startNonterm g `notElem` nonterminals g then error "Invalid starting symbol"
+    else g 
 
+checkRules :: Rules -> Bool
+checkRules [] = True
+checkRules (x:xs) = checkRules [x]
 
+-- ======================= Algorithm 1 =======================
+removeSimple :: Grammar -> Grammar
+removeSimple g = Grammar {nonterminals=nonterminals g, terminals=terminals g, startNonterm=startNonterm g, rules=newRules}
+    where newRules = createRules (createNa (nonterminals g) (rules g)) (rules g)
+ 
+
+-- TODO GET SIMPLE RULES BEFORE AND REMOVE SIMPLE RULES BEFORE createRules FUNCTION
+-- creates list of N_A sets for each nonterminal A
+createNa :: Nonterminals -> Rules -> NaSets
+createNa [] _ = []
+createNa (x:xs) rs =  (x, createNa' rs [x]) : createNa xs rs
+
+-- create one N_A set 
+createNa' ::  Rules -> [String] -> Nonterminals 
+createNa' rs nA0 =
+    if nA0 == nA1 then nA1
+    else createNa' rs nA1
+        where nA1 = nub (nA0 ++ concatMap snd (filter (\r -> fst r `elem` nA0) rs )) -- TODO change for union ? 
+
+-- get list of simple rules A->B
+getSimpleRules :: Rules -> Nonterminals -> Rules
+getSimpleRules rs nonts = filter ( \r -> any (`elem` nonts) (snd r) && length (snd r) == 1) rs
+
+createRules :: NaSets -> Rules -> Rules
+createRules [] _ = []
+createRules (x:xs) rs = createRules' x rs : createRules xs rs 
+
+createRules' :: NaSet -> Rules -> Rules
+createRules' nas rs = filter (\r -> fst r /= "") (map (\r -> if fst r `elem` snd nas then (fst nas, snd r) else ("",[])) rs)
+
+-- ===================== Load Grammar =======================
 main :: IO ()
 main = do 
     (action, content) <- parseArgs <$> getArgs
@@ -91,6 +132,6 @@ parseArgs (x:y:_)
 performAction :: String -> String -> IO String 
 performAction action content
     | action == "-i" = return $ show $ fmap checkSemantics ( parseGrammar content )
-    | action == "1" = return $ show $ checkSemantics $ Grammar {nonterminals=[], terminals=[content], startNonterm="S1", rules=[]}  
-    | action == "2" = return $ show $ Grammar {nonterminals=[], terminals=[content], startNonterm="S2", rules=[]}  
+    | action == "1" = return $ show $ fmap checkSemantics ( parseGrammar content )  
+    | action == "2" = return $ show $ fmap checkSemantics ( parseGrammar content )   
     | otherwise = error "Wrong argument" 
