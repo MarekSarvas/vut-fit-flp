e75f2639afd545a1bef717caa8f49e451b62dcf2
MarekSarvas <sarvas.sarvas@gmail.com>
2022-04-22 22:34:37 +0200

    io learning

--- project2/hamilton_cycle.pl
+++ project2/hamilton_cycle.pl
@@ -1,5 +1,18 @@
+read_line(L, C) :-
+    get_char(C),
+    (isEOFEOL(C), L = [], !; read_line(LL, _), [C|LL] = L).
+
+isEOFEOL(C) :-
+        C == end_of_file;
+        (char_code(C, Code), Code==10).
+
+read_lines(Ls) :-
+    read_line(L,C),
+    (C == end_of_file, Ls = [] ; read_lines(LLs), Ls = [L|LLs]).
+
 start :- 
-    write('This is sample Prolog program'),
-    write(' This program is written into hello_world.pl file'),
-    nl,
+    prompt(_, ''),
+    read_lines(LL),
+    %write(LL)
     halt.
+
