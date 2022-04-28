2d9275fb516ac49332af8d00e666a4b61d1ded13
MarekSarvas <sarvas.sarvas@gmail.com>
2022-04-28 22:39:05 +0200

    edges

--- project2/hamilton_cycle.pl
+++ project2/hamilton_cycle.pl
@@ -1,3 +1,11 @@
+
+remove_spaces([], []).
+remove_spaces([[A, _, B]|Ls], Rem) :- 
+    remove_spaces(Ls, Rem2), 
+    append([[A, B]], Rem2, Rem).
+    
+
+/**Prevzate z wis input2.pl: nacita riadky(hrany) zo standardneho vstupu a skonci na EOF alebo EOL */
 read_line(L, C) :-
     get_char(C),
     (isEOFEOL(C), L = [], !; read_line(LL, _), [C|LL] = L).
@@ -10,9 +18,11 @@ read_lines(Ls) :-
     read_line(L,C),
     (C == end_of_file, Ls = [] ; read_lines(LLs), Ls = [L|LLs]).
 
+/** main part */
 start :- 
     prompt(_, ''),
     read_lines(LL),
-    %write(LL)
+    remove_spaces(LL, S),
+    write(S),
     halt.
 
