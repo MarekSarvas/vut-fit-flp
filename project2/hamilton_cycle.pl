40c8a18fac44efd261ceba490f71c3e663a12dec
MarekSarvas <sarvas.sarvas@gmail.com>
2022-04-30 22:23:43 +0200

    one cycle

--- project2/hamilton_cycle.pl
+++ project2/hamilton_cycle.pl
@@ -17,6 +17,40 @@ remove_duplicates([], [], _).
 remove_duplicates([H|T1], [H|T2], Seen) :- \+ member(H, Seen), remove_duplicates(T1, T2, [H|Seen]).
 remove_duplicates([H|T1], T2, Seen) :- member(H, Seen), remove_duplicates(T1, T2, Seen).
 
+get_last([T], T).
+get_last([_|T], L) :-
+    get_last(T, L).
+
+/** create clauses for edge of the graph, both ways because they are not oriented */
+create_edges([]).
+create_edges([[X, Y]|T]) :-
+    assertz(edge(X, Y)),
+    assertz(edge(Y, X)),
+    create_edges(T).
+
+
+del_item([], _, []).
+del_item([H|T], H, New) :-
+    del_item(T, H, New).
+del_item([H|T], Item, New) :-
+    del_item(T, Item, NewRec),
+    append([H], NewRec, New).
+
+create_cycles([], [H|T], H, Cycle) :-
+    edge(H, Next),
+    last([H|T], Next),
+    append([Next], [H|T],Seen2),
+    Cycle = Seen2.
+
+create_cycles(Vertices, Seen, Prev, Cycle) :-
+    edge(Prev, NextV),
+    member(NextV, Vertices),
+    del_item(Vertices, NextV, NewVert),
+    create_cycles(NewVert, [NextV|Seen], NextV, Cycle).
+
+
+
+
 /**Prevzate z wis input2.pl: nacita riadky(hrany) zo standardneho vstupu a skonci na EOF alebo EOL */
 read_line(L, C) :-
     get_char(C),
@@ -37,6 +71,7 @@ start :-
     remove_spaces(LL, Edges),
     nodes(Edges, Nodes),
     remove_duplicates(Nodes, Cleaned),
+    create_clause(Edges),
     write(Edges),
     nl,
     write(Cleaned),
